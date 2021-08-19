1ec44dd4e37ce131684363584b02633f0bea8534
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-08-19 02:39:59 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+token = ''
+prefix = ''
+welcome_channel = ''
+member_role = ''
+guild_name = ''
+server_icon_link = ''
+server_link = ''
+mysql = {
+    'db': '',
+    'host': '127.0.0.1',
+    'password': '',
+    'user': ''
+}
